6f59b4065032e81cd2d193d2fbc5b6c1ed9a745f
Neha <67725607+nehasoni05@users.noreply.github.com>
2021-02-06 21:05:31 +0530

    Create beingGeeky.js

---
+++ Being_Geeky/public/SPA/beingGeeky.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./style.css";
+
+
+class BeingGeeky extends React.Component 
+{
+    render()
+    {
+        return (
+            <div className="jumbotron firstCon" id="geeky">
+            <h1 className="heading1">Being Geeky</h1>
+            <h2 className="heading2">Your own video and theortical study place.</h2>
+            <p className="container content1">Being Geeky is one of the best platform for the online learning. With the best study material.Dummy text is text that
+            is used in the publishing industry or by web designers to occupy the space which will later be filled with 'real' content. This is required when, for example,
+            the final text is not yet available. Dummy text is also known as 'fill text'. It is said that song composers of the past used dummy texts as lyrics when writing 
+            melodies in order to have a 'ready-made' text to sing with the melody. Dummy texts have been in use by typesetters since the 16th century.</p>
+            </div>
+        )
+    }
+}
+
+
+export default BeingGeeky;
