83c75f96fe19f279707555d6182439964c00a017
Neha <67725607+nehasoni05@users.noreply.github.com>
2021-02-06 21:10:41 +0530

    Create lectures.js

---
+++ Being_Geeky/public/SPA/lectures.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./style.css";
+
+
+class Lectures extends React.Component 
+{
+    render()
+    {
+        return (
+            <div className=" jumbotron four" style={{backgroundColor:'rgb(71, 111, 112)'}}>
+    <h2 className="heading3">Lectures available anywhere anytime.</h2>
+    <p className="content3">You can watch the lecture anytime from anywhere and ask doubts.The phrasal sequence of the Lorem Ipsum text is now so widespread and commonplace that
+    many DTP programmes can generate dummy text using the starting sequence "Lorem ipsum". Fortunately, the phrase 'Lorem Ipsum' is now recognized by electronic pre-press systems
+    and, when found, an alarm can be raised. This avoids a publication going to print with overlooked dummy text.</p>
+</div>
+        )
+    }
+}
+
+export default Lectures;
