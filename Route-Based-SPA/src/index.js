91419e0428621473d7fafb9208071d4f369a4876
Neha Soni <67725607+nehasoni05@users.noreply.github.com>
2021-03-29 20:32:32 +0530

    Completed the app

---
+++ Route-Based-SPA/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+
+class Cq extends React.Component {
+	render() {
+		return (
+			<App/> 
+		)
+	}
+}
+
+ReactDOM.render(
+
+        <Cq/>,
+	document.getElementById('root')
+);
