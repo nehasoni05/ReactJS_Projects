eda66b9425dfda7ee423d8ccc5eef83ac918c277
Neha Soni <67725607+nehasoni05@users.noreply.github.com>
2021-03-29 20:30:31 +0530

    Completed the app

---
+++ Route-Based-SPA/src/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Hashnode!!</h2>
+        <p>
+        Hashnode is the easiest way to start a developer blog on your  
+        personal domain 🌏 for free and connect with the readers through our  
+        global dev community! 👩‍💻👨‍💻
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
